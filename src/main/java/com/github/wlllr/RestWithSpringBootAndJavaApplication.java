785de8a0ad2e92ce33ddde4bd6037e50d0fadaae
Weller <weller_pereira@hotmail.com>
2025-06-06 16:35:15 -0300

    primeiro commit do projeto com Spring Boot and Java

---
+++ src/main/java/com/github/wlllr/RestWithSpringBootAndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.wlllr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaApplication.class, args);
+	}
+
+}
